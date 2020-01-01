5ae1846a4977a21f9fa5da00a78e7d2a99b6d6e1
Jason <jasonclairgamedev@gmail.com>
2020-01-01 17:43:55 -0500

    Starting testing on the DAOs

--- Bank/src/test/java/dev/jason/daostest/DAOTest.java
+++ Bank/src/test/java/dev/jason/daostest/DAOTest.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import dev.jason.daos.UserLocalDAO;
+import dev.jason.enities.User;
+import junit.framework.Assert;
+
 public class DAOTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createUserTest() {
+		UserLocalDAO useraccess = new UserLocalDAO();
+		
+		User phylicia = new User("babygirl", "12345", 0, null);
+		useraccess.createUser(phylicia);
+		User result = useraccess.getUserByUsername("babygirl");
+		
+		Assert.assertEquals("babygirl", result.getUsername());
+		System.out.println(result);
 	}
 
 }
